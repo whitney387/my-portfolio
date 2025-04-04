a990dddd3d9f71e2d8fecd34550fe971fc38114b
Whitney-Murugi <murugiwhitney01@gmail.com>
2025-04-04 23:12:55 +0300

    Update AboutMe.js

--- src/AboutMe.js
+++ src/AboutMe.js
@@ -5,10 +5,9 @@ export default function AboutMe() {
       <section id="about" class="container py-5">
         <h2>About Me</h2>
         <p>
-          I'm a second-year economics student with a growing passion for
-          software development. I specialize in web development, learning both
-          front-end and back-end technologies to create seamless user
-          experiences.
+          I'm a third year economics student with a growing passion for software
+          development. I specialize in web development, learning both front-end
+          and back-end technologies to create seamless user experiences.
         </p>
       </section>
     </div>
