7c0031356ee2b2087bbb59ec0b828c02c0e82055
Whitney-Murugi <murugiwhitney01@gmail.com>
2026-03-12 16:15:07 +0300

    updated html

--- src/AboutMe.js
+++ src/AboutMe.js
@@ -1,15 +1,23 @@
 import "./Interests.css";
+
 export default function AboutMe() {
   return (
-    <div>
-      <section id="about" class="container py-5">
-        <h2>About Me</h2>
-        <p>
-          I'm a third year economics student with a growing passion for software
-          development. I specialize in web development, learning both front-end
-          and back-end technologies to create seamless user experiences.
-        </p>
-      </section>
-    </div>
+    <section id="about" className="container py-5">
+      <h2>About Me</h2>
+
+      <p>
+        I am a junior front-end developer and a third-year Economics student
+        with a diploma in web development from
+        <strong> SheCodes </strong>. I am passionate about building clean,
+        responsive, and user-friendly web interfaces.
+
+        <br /><br />
+
+        I focus on learning modern front-end technologies, improving user
+        experience, and developing practical web solutions. I am eager to
+        contribute my growing technical skills, analytical background, and
+        willingness to learn in a professional development environment.
+      </p>
+    </section>
   );
 }
